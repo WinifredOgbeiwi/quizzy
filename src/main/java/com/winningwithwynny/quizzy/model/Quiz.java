b0f8021b81483e9ea8a5e967ba97078c4364ae66
winifredogbeiwi <imadeogbeiwi@gmail.com>
2025-05-20 11:14:35 +0200

    add quiz model

--- src/main/java/com/winningwithwynny/quizzy/model/Quiz.java
+++ src/main/java/com/winningwithwynny/quizzy/model/Quiz.java
@@ -1,4 +1,29 @@
 package com.winningwithwynny.quizzy.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity(name = "quizzes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "quizzes")
 public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    private String description;
+
+    @ElementCollection
+    private List<String> tags;
 }
